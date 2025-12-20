8412fe7bbcb9349fd9db30c5b98760426ad98242
Thomaz Maldonado Bonfim <127054536+Thomika1@users.noreply.github.com>
2025-12-20 18:20:57 -0300

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+echo "
+	     |\      _,,,---,,_
+      ZZZzz /,`.-'`'    -.  ;-;;,_
+           |,4-  ) )-,_. ,\ (  `'-'
+          '---''(_/--'  `-'\_)  Safira"
+
+function fish_greeting                                           
+    echo 'Hello!🤓'
+end 
+set _OLD_VIRTUAL_PATH "$PATH"
